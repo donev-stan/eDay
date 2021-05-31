070d6bddba46267665c8273fba8450ec0ec0be19
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-06-01 00:06:23 +0300

    layout & header styling / bootstrap / react-router

---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
+
+export const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Navbar.Brand>
+        <img
+          alt=""
+          src="./logo-bag.png"
+          width="42"
+          height="42"
+          className="d-inline-block align-center mb-2"
+        />{" "}
+        eDay
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link>All Sales</Nav.Link>
+          <Nav.Link>Sell Item</Nav.Link>
+          <Nav.Link>Sellers</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
